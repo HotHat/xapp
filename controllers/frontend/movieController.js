406f204eff892cc36baf87822ec4186a1a467edb
lyhux <774964107@qq.com>
2023-05-05 22:23:42 +0800

    add movie list

--- controllers/frontend/movieController.js
+++ controllers/frontend/movieController.js
@@ -3,9 +3,59 @@ const { sqlQuery, jsonFail, jsonSuccess, createCaptcha, password_hash, password_
 } = require(global.S.BASE_DIR + '/tools');
 
 
-exports.movie_list = function(req, res, next){
+exports.movie_list = function (req, res, next) {
+	let page = req.query.page || 1
 
-    res.json(jsonSuccess("dashboard"));
+	var p = parseInt(page)
+	if (isNaN(p)) {
+		p = 1
+	}
+
+	let perPage = parseInt(process.env.LIST_PER_PAGE)
+
+	let start = (p - 1) * perPage
+
+	let currentPage = sqlQuery(
+		"SELECT * FROM movie where thumb != '' ORDER BY site_id DESC LIMIT ? OFFSET ?",
+		[perPage, start]
+	)
+
+	// total count
+	let total = sqlQuery("select count(*) as count from movie where thumb != '' ")
+
+	let top5 = sqlQuery("select * from movie where thumb != '' order by site_id desc limit 5")
+
+	
+	Promise.all([currentPage, total, top5])
+	.then((results) => {
+		let lst = results[0]  
+		let cn = results[1]
+		let top5 = results[2]
+
+		var total = 0
+		if (cn.length > 0 ) {
+			total = cn[0].count
+		}
+
+		res.json({
+			code: 200,
+			data: {
+				list: lst,
+				top5: top5,
+				page: {
+					total_page: Math.ceil(total / perPage),
+					page_size: perPage,
+					current_page: page,
+					total: total
+				}
+			}
+		})
+		console.log(values);
+	})
+	.catch(function (err) {
+		next(err)
+	});
+
 }
 
 
--- routes/api/frontend.js
+++ routes/api/frontend.js
@@ -2,9 +2,17 @@ const express = require('express');
 const router = express.Router();
 const format = require('date-format');
 var { expressjwt } = require("express-jwt");
+const cors = require('cors')
 
 const movieController = require(global.S.BASE_DIR + '/controllers/frontend/movieController')
 
+var corsOptions = {
+    origin: 'http://127.0.0.1:5173',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+
+router.use(cors(corsOptions));
 
 // movie module
 router.get('/movie/list', movieController.movie_list)
